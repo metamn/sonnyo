5f2d984eef1cf4e78b089d3ce0ce479f699e8ebf
cs <bartus.csongor@gmail.com>
2015-10-06 12:12:52 +0300

    Weather data fetching done

--- site/components/project/weather/weather.js
+++ site/components/project/weather/weather.js
@@ -2,6 +2,8 @@ var weather = function(containerID) {
   var container = document.querySelector(containerID);
   var url = container.dataset.url;
 
+  // JS JSON AJAX Call to an API endpoint
+  // - http://www.w3schools.com/json/json_http.asp
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
@@ -9,7 +11,6 @@ var weather = function(containerID) {
   // Process request
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      console.log('response:' + xmlhttp.responseText);
       var result = JSON.parse(xmlhttp.responseText);
       parseResult(result);
     }
@@ -17,7 +18,12 @@ var weather = function(containerID) {
 
   // Process result
   function parseResult(result) {
-    console.log('result:' + result);
+    console.log('result:' + JSON.stringify(result));
+    var celsius = container.querySelector('.openweather__celsius .value');
+    celsius.innerHTML = result.main.temp;
+
+    var icon = container.querySelector('.openweather__icon .img');
+    icon.src += result.weather[0].icon + ".png";
   }
 }
 
