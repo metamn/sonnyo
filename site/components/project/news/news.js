424d87510170ee9066da173e1fc473878da7ea02
cs <bartus.csongor@gmail.com>
2015-10-09 09:15:29 +0300

    FB feed from JSON works

---
+++ site/components/project/news/news.js
@@ -0,0 +1,43 @@
+var news = function(containerID) {
+  var container = document.querySelector(containerID);
+  var url = container.dataset.url;
+
+  // JSON AJAX Call to an API endpoint
+  // - http://www.w3schools.com/json/json_http.asp
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
+  // Process request
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      console.log('fb:' + xmlhttp.responseText);
+      var result = JSON.parse(xmlhttp.responseText);
+      parseResult(result);
+    }
+  }
+
+  // Process result
+  function parseResult(result) {
+    var data = result.data;
+
+    for (var i = 0; i <= 4; i++) {
+      var item = document.createElement('div');
+      item.className = 'item';
+
+      var message = document.createElement('div');
+      message.className = 'item__message';
+      message.innerHTML = data[i].message;
+
+      var date = document.createElement('div');
+      date.className = 'item__date';
+      date.innerHTML = data[i].created_time;
+
+      item.appendChild(date);
+      item.appendChild(message);
+      container.appendChild(item);
+    }
+  }
+}
+
+news('.news .news__body');
