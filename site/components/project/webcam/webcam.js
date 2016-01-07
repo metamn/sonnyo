1e3e31d36f5b7bc5551ee0ea0dd7f35742bfdacb
cs <bartus.csongor@gmail.com>
2016-01-07 14:16:32 +0200

    Webcam added

--- site/components/project/webcam/webcam.js
+++ site/components/project/webcam/webcam.js
@@ -1,1 +1,31 @@
-videoResponsivePoster('.webcam');
+var webcam = function(containerID, triggerID, videoContainerID, closerID) {
+  var container = document.querySelector(containerID);
+  var trigger = document.querySelector(triggerID);
+  var closer = document.querySelector(closerID);
+  var videoContainer = document.querySelector(videoContainerID);
+
+  trigger.addEventListener('click', showVideo, false);
+  closer.addEventListener('click', closeVideo, false);
+
+  function showVideo() {
+    container.classList.add('webcam--active');
+
+    var url = videoContainer.dataset.url;
+
+    var iframe = document.createElement('iframe');
+    iframe.className = 'iframe';
+    iframe.setAttribute('src', url);
+
+    videoContainer.appendChild(iframe);
+  }
+
+  function closeVideo() {
+    container.classList.remove('webcam--active');
+
+    var iframe = videContainer.querySelector('.iframe');
+    videoContainer.remove(iframe);
+  }
+}
+
+
+webcam('.webcam', '.webcam__play', '.webcam__iframe', '.webcam__close');
