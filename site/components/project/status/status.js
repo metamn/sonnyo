5c7ad7c15ec2878d50b25d599323bbe1d9c496fc
metamn <bartus.csongor@gmail.com>
2018-01-30 10:02:17 +0200

    Status ok

--- site/components/project/status/status.js
+++ site/components/project/status/status.js
@@ -1,11 +1,22 @@
-var setStatus = function() {
+var setStatus = function(ID, klassname) {
   var ref = firebase.database().ref('/status');
-  console.log('ref:' + ref);
 
   ref.once("value").then(function(snapshot) {
-    var key = JSON.stringify(snapshot);
-    console.log('status:' + key);
+    var key = snapshot.val();
+    var klass = klassname + '--' + key;
+    console.log('klass:' + klass);
+
+    var messages = document.querySelectorAll(ID);
+    for (var i = 0; i < messages.length; i++) {
+      if (messages[i].classList.contains(klass)) {
+        messages[i].classList.remove(klassname + '--hidden');
+        messages[i].classList.add(klassname + '--visible');
+      } else {
+        messages[i].classList.remove(klassname + '--visible');
+        messages[i].classList.add(klassname + '--hidden');
+      }
+    }
   });
 }
 
-setStatus();
+setStatus('.status .status__message', 'status__message');
