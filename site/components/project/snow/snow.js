9234903b4f0a6f450ceb0e927a841cff35d064c0
metamn <bartus.csongor@gmail.com>
2018-01-30 10:44:02 +0200

    Program works in status

---
+++ site/components/project/snow/snow.js
@@ -0,0 +1,15 @@
+var setSnow = function(ID) {
+  var ref = firebase.database().ref('/snow');
+
+  ref.once("value").then(function(snapshot) {
+    var val = snapshot.val();
+    console.log('snow:' + val);
+
+    var snow = document.querySelector(ID);
+    if (snow) {
+      snow.innerHTML = val + 'cm';
+    }
+  });
+}
+
+setSnow('.snow .cm');
